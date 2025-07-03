07a5ab3bf351e2e54da5a1cf6b9f725c7226250d
gislaine-15 <146885932+gislaine-15@users.noreply.github.com>
2025-07-03 11:16:43 -0300

    site

--- script.js
+++ script.js
@@ -4,3 +4,83 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado= document.querySelector("caixa-resultado");
 const textoResultado= document.querySelector("texto-resultado");
 
+const perguntas = [
+    {
+      enunciao: "Quantos jogadores compõem uma equipe de voleibol em quadra durante uma partida?",
+      alternativas: [
+        {
+            texto:"6",
+            afirmação:"resposta correta",
+            pontos:1
+        },
+        {
+            texto:"7",
+            afirmação:"resposta incorreta",
+            pontos:0
+        }
+      ]
+    },
+    {
+      enunciao: "Qual é o nome do movimento usado para começar o jogo no voleibol?",
+      alternativas: [
+        {
+            texto:"Batida",
+            afirmação:"resposta incorreta",
+            pontos:0
+        },
+        {
+            texto:"Saque",
+            afirmação:"resposta correta",
+            pontos:1
+        }
+      ]
+    },
+     {
+      enunciao: "Quantos sets um jogo de voleibol geralmente possui?",
+      alternativas: [
+        {
+            texto:"5",
+            afirmação:"resposta incorreta",
+            pontos:0
+        },
+        {
+            texto:"3",
+            afirmação:"resposta correta",
+            pontos:1
+        }
+      ]
+    },
+{
+      enunciao: "Qual é o nome do jogador responsável por distribuir a bola para os atacantes durante o jogo?",
+      alternativas: [
+        {
+            texto:"Levantador",
+            afirmação:"resposta correta",
+            pontos:1
+        },
+        {
+            texto:"Atacante",
+            afirmação:"resposta incorreta",
+            pontos:0
+        }
+      ]
+    },
+    {
+      enunciao: " Qual é a marcação numérica de pontos necessária para vencer um set no voleibol?",
+      alternativas: [
+        {
+            texto:"30",
+            afirmação:"resposta incorreta",
+            pontos:0
+        },
+        {
+            texto:"25",
+            afirmação:"resposta correta",
+            pontos:1
+        }
+      ]
+    },
+
+]
+
+
