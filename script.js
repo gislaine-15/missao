b9d6d1c27954624f145ef62db7821289c8544d32
gislaine-15 <146885932+gislaine-15@users.noreply.github.com>
2025-08-07 11:09:59 -0300

    site

--- script.js
+++ script.js
@@ -92,8 +92,14 @@ function mostraPergunta(){
   caixaPerguntas.textContent=perguntaAtual.enunciado;
   caixaAlternativas.textContent="";
   mostraAlternativas();
-  
-
+}
+function mostraAlternativas(){
+  for(const alternativa of perguntaAtual.alternativas){
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa.texto;
+    botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
 }
 
 
